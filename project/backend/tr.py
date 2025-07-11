29d119772e671be61bad8f8fc162a9270feb6645
Arpit Mishra <113346036+TheChaoticor@users.noreply.github.com>
2025-07-11 22:42:27 +0530

    Update tr.py

--- project/backend/tr.py
+++ project/backend/tr.py
@@ -3,8 +3,7 @@ import torch
 checkpoint = torch.load("ffpp_c40.pth", map_location="cpu")
 print("Checkpoint Keys:", checkpoint.keys())
 
-# If the model's state_dict is inside a "state_dict" key:
 if "state_dict" in checkpoint:
-    print("State Dict Keys:", list(checkpoint["state_dict"].keys())[:10])  # First 10 keys
+    print("State Dict Keys:", list(checkpoint["state_dict"].keys())[:10])  
 else:
-    print("State Dict Keys:", list(checkpoint.keys())[:10])  # First 10 keys
+    print("State Dict Keys:", list(checkpoint.keys())[:10]) 
